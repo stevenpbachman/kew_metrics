8eeb7e0130a7ff87b82b26cd3db44cb5e4b3a65e
Keith Newman <keithnewman@users.noreply.github.com>
2025-03-21 15:35:13 +0000

    feat: prepare method to globally import data from CSV and shapefiles

--- app.R
+++ app.R
@@ -19,13 +19,6 @@ library(readr)
 # load all data
 # load_data <- function() {
 #   list(
-EDGEspecies <- read_csv("01_data/EDGE/EDGEspecies_matched.csv")
-EDGEcountries <- read_csv("01_data/EDGE/edge_ranges.csv")
-SampledGlobal <- read_csv("01_data/RedList/SRLI_2024.csv")
-metrics_gbf <- read_csv("03_docs/metrics_gbf.csv")
-tipas <- read_csv("01_data/TIPAS/TIPAs.csv")
-tipas_shp <- st_read("01_data/TIPAS/TIPA_Composite_POLYGON/TIPA_Composite_POLYGON.shp") %>% 
-st_zm(drop = TRUE, what = "ZM")
 #   )
 # }
 
