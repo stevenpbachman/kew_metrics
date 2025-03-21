2585e77d5669881366543cdfbe84ea24f98abcc7
Keith Newman <keithnewman@users.noreply.github.com>
2025-03-21 15:36:53 +0000

    feat: implement new app.R and run() methods, inspired by leprechaun method

--- app.R
+++ app.R
@@ -1,32 +1,9 @@
+# Launch the ShinyApp
+# do not remove to keep push deploy button
+# from RStudio
+pkgload::load_all(
+  reset = TRUE,
+  helpers = FALSE
+)
 
-# libraries
-library(shiny)
-library(bslib)
-library(bsicons)
-library(DT)
-library(leaflet)
-library(ggplot2)
-library(dplyr)
-library(sysfonts)
-library(scales)
-library(sf)
-library(httr)
-library(plotly)
-library(mapview)
-library(ggiraph)
-library(readr)
-
-# load all data
-# load_data <- function() {
-#   list(
-#   )
-# }
-
-# Source the UI and server files
-source("ui.R")
-source("server.R")
-
-# Run the Shiny app
-shinyApp(ui = ui, server = server)
-
-
+run()
