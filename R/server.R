2de906b16a5092871728311e7d98c01d43c81521
Keith Newman <keithnewman@users.noreply.github.com>
2025-03-25 18:47:25 +0000

    fix: Remove redundant data-selection code from server

--- R/server.R
+++ R/server.R
@@ -4,113 +4,12 @@
 #' @param session A shiny session object.
 #' @return A Shiny server.
 server <- function(input, output, session) {
+  # Add module servers ----
 
-  # Dataset manipulation ----
+  ## Diversity ----
+  conservation_server(id = "conservation")
 
-  # Reactive to control which dataset is being used
-  base_data <- reactive({
-    if (!is.null(input$dataset1) && input$dataset1 != "") {
-      switch(
-        input$dataset1,
-        "edgespecies" = EDGEspecies,
-        "edgecountries" = EDGEcountries,
-        "edgeindex" = "",
-        "edgezones" = ""
-      )
-    } else if (!is.null(input$dataset2) && input$dataset2 != "") {
-      switch(input$dataset2,
-             "globalsampled" = SampledGlobal)
-    } else if (!is.null(input$dataset3) &&
-               input$dataset3 != "") {
-      switch(input$dataset3, "tipas" = tipas)
-    } else {
-      NULL
-    }
-  })
-
-  # Reactive expression to determine which dataset type is selected
-  dataset_type <- reactive({
-    if (!is.null(input$dataset1) && input$dataset1 != "") {
-      return("edge")
-    }
-    if (!is.null(input$dataset2) && input$dataset2 != "") {
-      return("redlist")
-    }
-    if (!is.null(input$dataset3) && input$dataset3 != "") {
-      return("tipas")
-    }
-    return(NULL)
-  })
-
-  # Reactive to control filtered data in the data tables
-  selected_data <- reactive({
-    req(base_data())
-    req(dataset_type())
-
-    current_data <- base_data()
-
-    switch(
-      dataset_type(),
-      "edge" = {
-        if (input$dataset1 == "edgespecies") {
-          if (!is.null(input$data_table_edgespecies_rows_all)) {
-            current_data[input$data_table_edgespecies_rows_all, ]
-          } else {
-            current_data
-          }
-        } else if (input$dataset1 == "edgecountries") {
-          if (!is.null(input$data_table_edgeranges_rows_all)) {
-            current_data[input$data_table_edgeranges_rows_all, ]
-          } else {
-            current_data
-          }
-        } else {
-          current_data
-        }
-      },
-      "redlist" = {
-        if (!is.null(input$data_table_redlist_rows_all)) {
-          current_data[input$data_table_redlist_rows_all, ]
-        } else {
-          current_data
-        }
-      },
-      "tipas" = {
-        if (!is.null(input$data_table_tipas_rows_all)) {
-          current_data[input$data_table_tipas_rows_all, ]
-        } else {
-          current_data
-        }
-      },
-      current_data  # default case
-    )
-  })
-
-  # Automatically reset other selection to "None" when one dataset is selected
-  observeEvent(input$dataset1, {
-    if (input$dataset1 != "") {
-      updateSelectInput(session, "dataset2", selected = "")
-      updateSelectInput(session, "dataset3", selected = "")
-    }
-  })
-
-  observeEvent(input$dataset2, {
-    if (input$dataset2 != "") {
-      updateSelectInput(session, "dataset1", selected = "")
-      updateSelectInput(session, "dataset3", selected = "")
-    }
-  })
-
-  observeEvent(input$dataset3, {
-    if (input$dataset3 != "") {
-      updateSelectInput(session, "dataset1", selected = "")
-      updateSelectInput(session, "dataset2", selected = "")
-    }
-  })
-
-  # Diveristy UI ----
-
-  # Risk UI ----
+  ## Risk ----
   risk_server(id = "risk")
 
   ## GBF ----
