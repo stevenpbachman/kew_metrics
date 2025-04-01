f178700bb6fdf6b215fd04643a61fe20fa0d4416
Keith Newman <keithnewman@users.noreply.github.com>
2025-04-01 14:54:48 +0100

    fix: Remove benchmarking code

--- inst/02_data_prep/convert_to_parquet_format.R
+++ inst/02_data_prep/convert_to_parquet_format.R
@@ -40,29 +40,3 @@ write_parquet(
   redlist_srli,
   file.path(redlist_dir, "SRLI_2024.parquet")
 )
-#
-
-
-
-bench::mark(
-  readr::read_csv(file.path(redlist_dir, "SRLI_2024.csv")),
-  arrow::read_parquet(file.path(redlist_dir, "SRLI_2024.parquet")),
-  check = FALSE
-)
-
-bench::mark(
-  readr::read_csv(
-    file.path(edge_dir, "edge_ranges.csv"),
-    col_types = readr::cols(
-      accepted_plant_name_id = readr::col_integer(),
-      plant_locality_id = readr::col_integer(),
-      continent_code_l1 = readr::col_integer(),
-      region_code_l2 = readr::col_integer(),
-      introduced = readr::col_integer(),
-      extinct = readr::col_integer(),
-      location_doubtful = readr::col_integer()
-    )
-  ),
-  arrow::read_parquet(file.path(edge_dir, "edge_ranges.parquet")),
-  check = FALSE
-)
